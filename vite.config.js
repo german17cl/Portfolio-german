7d819c50944dd57853cf5162a2ba69dcf945b860
german17cl <germancamp17@gmail.com>
2026-06-05 01:53:46 +0200

    vuelve

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-
-export default {
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-}
 export default defineConfig({
   plugins: [
     vue(),
