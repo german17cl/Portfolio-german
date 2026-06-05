ea51a5c14993771e794a3f75ad493d7f67908c51
german17cl <germancamp17@gmail.com>
2026-06-05 02:37:03 +0200

    ya

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Portfolio-german/',
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
